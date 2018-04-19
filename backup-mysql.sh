f59172ff7c68c155c69d503dadbc4c133150ca7d
ProCycleDev <18075290+ProCycleDev@users.noreply.github.com>
2018-04-19 10:17:37 -0700

    Added ${database_name} to path

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -8,7 +8,7 @@ backup_owner="$3"
 parent_dir="/backups/mariadb"
 defaults_file="/etc/mysql/${database_name}-backup.cnf"
 todays_dir="$(date +%F)"
-log_file="${parent_dir}/${todays_dir}/backup-progress.log"
+log_file="${parent_dir}/${database_name}/${todays_dir}/backup-progress.log"
 now="$(date +%FT%H%MZ)"
 processors="$(nproc --all)"
 
@@ -31,7 +31,7 @@ set_options () {
     # List the innobackupex arguments
     innobackupex_args=(
         "--defaults-file=${defaults_file}"
-        "--extra-lsndir=${parent_dir}/${todays_dir}"
+        "--extra-lsndir=${parent_dir}/${database_name}/${todays_dir}"
         "--backup"
         "--compress"
         "--stream=xbstream"
@@ -43,39 +43,39 @@ set_options () {
 
     # Add option to read LSN (log sequence number) if a full backup has been
     # taken today.
-    if grep -q -s "to_lsn" "${parent_dir}/${todays_dir}/xtrabackup_checkpoints"; then
+    if grep -q -s "to_lsn" "${parent_dir}/${database_name}/${todays_dir}/xtrabackup_checkpoints"; then
         backup_type="incremental"
-        lsn=$(awk '/to_lsn/ {print $3;}' "${parent_dir}/${todays_dir}/xtrabackup_checkpoints")
+        lsn=$(awk '/to_lsn/ {print $3;}' "${parent_dir}/${database_name}/${todays_dir}/xtrabackup_checkpoints")
         innobackupex_args+=( "--incremental-lsn=${lsn}" )
     fi
 }
 
 take_backup () {
     # Make sure today's backup directory is available and take the actual backup
-    mkdir -p "${parent_dir}/${todays_dir}"
-    find "${parent_dir}/${todays_dir}" -type f -name "*.incomplete" -delete
-    mariabackup "${innobackupex_args[@]}" "--target-dir=${parent_dir}/${todays_dir}" > "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream.incomplete" 2> "${log_file}"
+    mkdir -p "${parent_dir}/${database_name}/${todays_dir}"
+    find "${parent_dir}/${database_name}/${todays_dir}" -type f -name "*.incomplete" -delete
+    mariabackup "${innobackupex_args[@]}" "--target-dir=${parent_dir}/${database_name}/${todays_dir}" > "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream.incomplete" 2> "${log_file}"
     
-    mv "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream.incomplete" "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream"
+    mv "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream.incomplete" "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream"
 }
 
 upload_backup () {
     # Upload the backup file to an S3 compatible provider using s3cmd
-	if [ -e "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream" ]
+	if [ -e "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream" ]
 	then
-		if [ -s "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream" ]
+		if [ -s "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream" ]
 		then
-			if [ -r "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream" ]
+			if [ -r "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream" ]
 			then
-				s3cmd -c "/etc/mysql/${s3_space_name}.s3cfg" -e put "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream" "s3://${s3_space_name}/$HOSTNAME/${database_name}/${todays_dir}/"
+				s3cmd -c "/etc/mysql/${s3_space_name}.s3cfg" -e put "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream" "s3://${s3_space_name}/$HOSTNAME/${database_name}/${todays_dir}/"
 			else
-				echo "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream is not readable"
+				echo "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream is not readable"
 			fi
 		else
-			echo "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream is zero in size"
+			echo "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream is zero in size"
 		fi
 	else
-		echo "${parent_dir}/${todays_dir}/${backup_type}-${now}.xbstream does not exist"
+		echo "${parent_dir}/${database_name}/${todays_dir}/${backup_type}-${now}.xbstream does not exist"
 	fi
 }
 
@@ -84,7 +84,7 @@ sanity_check && set_options && take_backup && upload_backup
 # Check success and print message
 if tail -1 "${log_file}" | grep -q "completed OK"; then
     printf "Backup successful!\n"
-    printf "Backup created at %s/%s-%s.xbstream\n" "${parent_dir}/${todays_dir}" "${backup_type}" "${now}"
+    printf "Backup created at %s/%s-%s.xbstream\n" "${parent_dir}/${database_name}/${todays_dir}" "${backup_type}" "${now}"
 else
     error "Backup failure! Check ${log_file} for more information"
 fi
